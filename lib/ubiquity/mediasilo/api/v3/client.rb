18a3c0328dc29db9242f7d334a19eb8073e38c59
John Whitson <john.whitson@gmail.com>
2014-12-26 06:47:04 -0700

    Started work on error message handling and success detection

--- lib/ubiquity/mediasilo/api/v3/client.rb
+++ lib/ubiquity/mediasilo/api/v3/client.rb
@@ -5,7 +5,7 @@ class Ubiquity::MediaSilo::API::V3::Client
 
   attr_accessor :logger
 
-  attr_accessor :http_client, :response
+  attr_accessor :http_client, :request, :response
 
   def initialize(args = { })
     initialize_logger(args)
@@ -26,20 +26,36 @@ class Ubiquity::MediaSilo::API::V3::Client
     @http_client = Ubiquity::MediaSilo::API::V3::HTTPClient.new(args)
   end
 
+  def error_message
+    ''
+  end
+
+  def success?
+    _code = http_client.response.code
+    _code and _code.start_with?('2')
+  end
+
   def process_request(request_class, args, options = { })
-    puts request_class
-    _request = request_class.new(args, options.merge(:client => self))
-    options[:query] = _request.query
+    @request = request_class.new(args, options.merge(:client => self))
+    options[:query] = request.query
     @response = case request_class::HTTP_METHOD
                   when :delete
-                    http_client.delete(_request.path, options)
+                    http_client.delete(request.path, options)
                   when :put
-                    http_client.put(_request.path, _request.body, options)
+                    http_client.put(request.path, request.body, options)
                   when :post
-                    http_client.post(_request.path, _request.body, options)
+                    http_client.post(request.path, request.body, options)
                   when :get
-                    http_client.get(_request.path, _request.query, options)
+                    http_client.get(request.path, request.query, options)
                 end
+  end
+
+  def asset_copy_to_folder(args = { }, options = { })
+    process_request(Requests::AssetCopyToFolder, args, options)
+  end
+
+  def asset_copy_to_project(args = { }, options = { })
+    process_request(Requests::AssetCopyToProject, args, options)
   end
 
   def asset_create(args = { }, options = { })
@@ -64,14 +80,29 @@ class Ubiquity::MediaSilo::API::V3::Client
     process_request(Requests::AssetGetById, args, options)
   end
 
-  def assets_get(args = { })
-    @response = http_client.get('/assets')
+  def assets_get(args = { }, options = { })
+    query = options[:query] || { :type => '{"in":"video,image,document,archive,audio"}' }
+    @response = http_client.get('/assets', query)
   end
 
-
-  def assets_get_by_project_id(project_id)
-    project_id = project_id[:id] || project_id[:project_id] if project_id.is_a?(Hash)
+  # @note This will get all assets for a project, even if they are in a folder
+  # @param [Hash] args
+  # @option args [String] project_id The Id of the project to get the assets for
+  # @return [Array]
+  def assets_get_by_project_id(args = { }, options = { })
+    project_id = case args
+                   when String; args
+                   when Hash; args[:id] || args[:project_id]
+                 end
     @response = http_client.get('/projects/%s/assets' % project_id)
+  end
+
+  def asset_move_to_folder(args = { }, options = { })
+    process_request(Requests::AssetMoveToFolder, args, options)
+  end
+
+  def asset_move_to_project(args = { }, options = { })
+    process_request(Requests::AssetMoveToProject, args, options)
   end
 
   def folder_create(args = { })
@@ -81,6 +112,22 @@ class Ubiquity::MediaSilo::API::V3::Client
   def folder_delete(folder_id)
     folder_id = folder_id[:id] if folder_id.is_a?(Hash)
     @response = http_client.delete('/folders/%s' % folder_id)
+  end
+
+  def folders_get_by_parent_id(args = { })
+    folder_id = case args
+                   when String; args
+                   when Hash; args[:id] || args[:parent_id]
+                 end
+    @response = http_client.get('/folders/%s/subfolders' % folder_id)
+  end
+
+  def folders_get_by_project_id(args = { })
+    project_id = case args
+                   when String; args
+                   when Hash; args[:id] || args[:project_id]
+                 end
+    @response = http_client.get('/projects/%s/folders' % project_id)
   end
 
   def project_create(args = { })

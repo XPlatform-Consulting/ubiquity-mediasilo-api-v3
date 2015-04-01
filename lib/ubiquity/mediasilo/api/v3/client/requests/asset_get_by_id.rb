b12bb193510a280f89d698ea08f485afd31d9136
John Whitson <john.whitson@gmail.com>
2015-04-01 11:58:14 -0600

    Fixed asset_get_by_id request

--- lib/ubiquity/mediasilo/api/v3/client/requests/asset_get_by_id.rb
+++ lib/ubiquity/mediasilo/api/v3/client/requests/asset_get_by_id.rb
@@ -3,10 +3,10 @@ module Ubiquity::MediaSilo::API::V3::Client::Requests
   class AssetGetById < BaseRequest
 
     HTTP_METHOD = :get
-    HTTP_PATH = '/assets/#{arguments[:assetId]}'
+    HTTP_PATH = '/assets/#{arguments[:asset_id]}'
 
     PARAMETERS = [
-      { :name => :asset_id, :aliases => [ :id ], :required => true },
+      { :name => :asset_id, :aliases => [ :id ], :send_in => :path, :required => true },
     ]
 
   end

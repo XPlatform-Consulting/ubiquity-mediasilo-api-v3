63b18bd554d4cc8820aab1bae4b5edc078edb17b
John Whitson <john.whitson@gmail.com>
2015-04-03 12:54:55 -0600

    Added batch and quicklink request classes

--- lib/ubiquity/mediasilo/api/v3/client/requests.rb
+++ lib/ubiquity/mediasilo/api/v3/client/requests.rb
@@ -10,3 +10,8 @@ require 'ubiquity/mediasilo/api/v3/client/requests/asset_move_to_project'
 
 require 'ubiquity/mediasilo/api/v3/client/requests/assets_get_by_folder_id'
 require 'ubiquity/mediasilo/api/v3/client/requests/assets_get_by_project_id'
+
+require 'ubiquity/mediasilo/api/v3/client/requests/batch'
+
+require 'ubiquity/mediasilo/api/v3/client/requests/quicklink_create'
+require 'ubiquity/mediasilo/api/v3/client/requests/quicklink_share'

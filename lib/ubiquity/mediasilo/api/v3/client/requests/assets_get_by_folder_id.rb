53f651944614a4168ef7b956dcc28b8dbf3a7955
John Whitson <john.whitson@gmail.com>
2015-06-19 15:44:58 -0600

    Fixed paths for assets_get_by_folder_id and assets_get_by_project_id

--- lib/ubiquity/mediasilo/api/v3/client/requests/assets_get_by_folder_id.rb
+++ lib/ubiquity/mediasilo/api/v3/client/requests/assets_get_by_folder_id.rb
@@ -3,7 +3,7 @@ module Ubiquity::MediaSilo::API::V3::Client::Requests
   class AssetsGetByFolderId < BaseRequest
 
     HTTP_METHOD = :get
-    HTTP_PATH = '/assets/#{arguments[:folderId]}'
+    HTTP_PATH = '/folders/#{arguments[:folderId]}/assets'
 
     PARAMETERS = [
       { :name => :folderId, :aliases => [ :id ], :required => true },
--- lib/ubiquity/mediasilo/api/v3/client/requests/assets_get_by_project_id.rb
+++ lib/ubiquity/mediasilo/api/v3/client/requests/assets_get_by_project_id.rb
@@ -3,7 +3,7 @@ module Ubiquity::MediaSilo::API::V3::Client::Requests
   class AssetsGetByProjectId < BaseRequest
 
     HTTP_METHOD = :get
-    HTTP_PATH = '/assets/#{arguments[:projectId]}'
+    HTTP_PATH = '/projects/#{arguments[:projectId]}/assets'
 
     PARAMETERS = [
       { :name => :projectId, :aliases => [ :id ], :required => true },
